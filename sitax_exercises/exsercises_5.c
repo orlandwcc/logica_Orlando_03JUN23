9bced1ba9da818d07bce42726252d46822e8bbbf
orlandwcc <orlandwcc@gmail.com>
2023-06-03 17:36:56 -0400

    modificacion

---
+++ sitax_exercises/exsercises_5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    // Este es un comentario de una linea
+    printf("Hola Mundo");// Este tambien
+
+    /*
+    Este es un comentario multilinea
+    y esta linea de mas 
+    es par ver modificaciones
+    ya creadas y subirlas a github
+    */
+return 0;
+}
