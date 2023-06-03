a74db6311a7443403cb89f8d3c3c076685cad315
orlandwcc <orlandwcc@gmail.com>
2023-06-03 17:55:04 -0400

    creacion archivo exercises_5.c

---
+++ sitax_exercises/exercises_5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    Variables en C 
+
+    Las variables son contenedores para almacenar valores de datos, como numeros y caracteres. 
+
+    En C, hay diferentes tipos de variables (definidas por diferentes palabras), por ejemplo:
+    
+    *Int - almacena enteros (numeros enteros), sin decimales, como 123 o -123
+    *Float - almacena numeros de coma flotantes o decimales, como 19.99 o -19.99
+    *Char - almacena caracteres individuales, como 'a' o 'b'. Los valores de char estan entre comillas simples 
+    */
+
+    /*
+    Declaracion (crear variables) de variables
+
+    Para crear una variable en C especifique el tipo y asignele un valor:
+
+    *sintaxis
+
+    tipo nombreVariable = valor
+
+    Donde tipo es uno de los tipos de variables en C (Como int) y nombreVariable es el nombre de la variable (Como x o miNombre). El signo igual se utiliza para asignar un valor de una variable.
+
+    */
+    
+    //Crear una variable que almacene un nimero entero.
+
+    int Minumero=15;
+return 0;
+}
