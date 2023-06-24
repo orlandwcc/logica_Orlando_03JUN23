1f778b13ee8861a92eef21bb4352c77b1647d7c6
orlandwcc <orlandwcc@gmail.com>
2023-06-23 22:36:54 -0400

    commit exsercises

---
+++ sitax_exercises/Exercises_Practice/ejercicio_5.c
@@ -0,0 +1,28 @@
+/*
+5. Hacer una funcion que diga si un numero es primo
+*/
+
+# include <stdio.h>
+
+int main() {
+	int n, x, num=0;
+	printf("Ingresa un rango de numero: ");
+	scanf("%i", &n);
+
+	while (n>0) {
+		num++;
+		x = 1;
+		int count = 0;
+		while (x<=num) {
+			if (num%x==0) {
+				count++;
+			}
+			x++;
+		}
+		if (count==2) {
+			printf("El numero %i es primo\n", num);
+			n--;
+		}
+	}
+	return 0;
+}
